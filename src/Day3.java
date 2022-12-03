59bf78123b0769d9533b742d05f70d94f12695be
Leia Cellfish <ninius@online.no>
2022-12-03 06:30:27 +0100

    Day 3 done! Java was nice today..

--- src/Day3.java
+++ src/Day3.java
@@ -31,10 +31,82 @@ public class Day3 {
 
 	private void part1(ArrayList<String> data){
 
+		int sum = 0;
+
+		for (String string : data) { sum += findCommon(string); }
+
+		System.out.println("The sum of priorities is " + sum);
+
+	}
+
+	private int findCommon(String rucksack){
+
+		int split = rucksack.length() / 2;
+
+		String first = rucksack.substring(0, split);
+		String second = rucksack.substring(split);
+
+		char common = ' ';
+
+		for (int i = 0; i < first.length(); i++){
+
+			if (second.indexOf(first.charAt(i)) != -1){
+
+				common = first.charAt(i);
+				break;
+
+			}
+
+		}
+
+		return findValue(common);
+	}
+
+	private int findValue(char common){
+
+		int ascii = common;
+
+		if (Character.isUpperCase(common)){
+			ascii = ascii - 65 + 27;
+		} else {
+			ascii = ascii - 97 + 1;
+		}
+
+		return ascii;
+
 	}
 
 	private void part2(ArrayList<String> data){
 
+		int sum = 0;
+
+		for (int i = 0; i < data.size(); i += 3){
+
+			sum += findCommonThree(data.get(i), data.get(i+1), data.get(i+2));
+
+		}
+
+		System.out.println("The sum of all badges is " + sum);
+
 	}
 
+	private int findCommonThree(String first, String second, String third) {
+
+		char common = ' ';
+
+		for (int i = 0; i < first.length(); i++){
+
+			if (second.indexOf(first.charAt(i)) != -1 && third.indexOf(first.charAt(i)) != -1 ){
+
+				common = first.charAt(i);
+				break;
+
+			}
+
+		}
+
+		return findValue(common);
+
+	}
+	
 }
