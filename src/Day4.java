56168456c6d7272dd33e25cd251f36866c7bd0e8
Leia Cellfish <ninius@online.no>
2022-12-04 06:28:24 +0100

    Where is the bug

--- src/Day4.java
+++ src/Day4.java
@@ -18,7 +18,7 @@ public class Day4 {
 		System.out.println();
 
 		part1(testinput);
-		part1(input);
+		//part1(input);
 
 		System.out.println();
 		System.out.println("Part 2: ");
@@ -30,6 +30,48 @@ public class Day4 {
 	}
 
 	private void part1(ArrayList<String> data){
+
+		int sum = 0;
+
+		for (String input : data) {
+			
+			String[] temp = input.split(",");
+
+			if (unpackSections(temp)) { sum++;}
+
+		}
+
+		System.out.println("The amount of sections fully contained are " + sum);
+
+	}
+
+	private boolean unpackSections(String[] input){
+
+		String[] first = input[0].split("-");
+		String[] second = input[1].split("-");
+
+		StringBuilder sectionOne = new StringBuilder();
+		StringBuilder sectionTwo = new StringBuilder();
+
+		for (int i = Integer.parseInt(first[0]); i <= Integer.parseInt(first[1]); i++){
+
+			sectionOne.append(i);
+
+		}
+
+		for (int i = Integer.parseInt(second[0]); i <= Integer.parseInt(second[1]); i++){
+
+			sectionTwo.append(i);
+
+		}
+
+		return contains(sectionOne, sectionTwo);
+			
+	}
+
+	private boolean contains(StringBuilder sectionOne, StringBuilder sectionTwo){
+
+		return sectionOne.toString().contains(sectionTwo.toString()) || sectionTwo.toString().contains(sectionOne.toString());
 
 	}
 
