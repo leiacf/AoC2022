91c96628f90dca7993e3d960408682a145402534
Nina Høegh-Larsen <ninius@online.no>
2022-12-06 06:38:48 +0100

    Day 6 done. Nifty!

--- src/Day6.java
+++ src/Day6.java
@@ -31,10 +31,58 @@ public class Day6 {
 
 	private void part1(ArrayList<String> data){
 
+		int sum = 0;
+
+		for (String string : data) {
+			sum = findMarker(string, 4);
+			System.out.println("The first marker is found at place " + sum);
+		}
+
 	}
 
 	private void part2(ArrayList<String> data){
 
+		int sum = 0;
+
+		for (String string : data) {
+			sum = findMarker(string, 14);
+			System.out.println("The first marker is found at place " + sum);
+		}
+
+	}
+
+	private int findMarker(String string, int size) {
+
+		ArrayList<Character> characters = new ArrayList<>();
+
+		int i = 0;
+
+		for (; i < string.length(); i++){
+
+			char temp = string.charAt(i);
+
+			if (characters.contains(temp)){
+
+				int remove = characters.indexOf(temp);
+
+				for (int j = remove; j >= 0 ; j--){
+					characters.remove(j);
+				}
+
+			}
+
+			characters.add(temp);
+
+			if (characters.size() == size){
+				break;
+			}
+
+		}
+
+		i++;
+
+		return i;
+		
 	}
 
 }
