9d49bf551dc38765277c47cd144cf4589d09fab3
Nina Høegh-Larsen <ninius@online.no>
2022-12-08 07:27:25 +0100

    Day 8 done! Have to skip day 7 for now..

--- src/Day8.java
+++ src/Day8.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day8 {
 
@@ -31,9 +32,191 @@ public class Day8 {
 
 	private void part1(ArrayList<String> data){
 
+		int sum = 0;
+		int[][] grid = parseData(data);
+
+		//print(grid);
+
+		sum = countHiddenTrees(grid);
+
+		System.out.println("The sum of hidden trees are: " + sum);
+
 	}
+
+	private int[][] parseData(ArrayList<String> data){
+
+		int y = data.size();
+		int x = data.get(0).length();
+
+		int[][] grid = new int[y][x];
+
+		for (int i = 0;  i < data.size(); i++) {
+			
+			String line = data.get(i);
+
+			for (int j = 0; j < line.length(); j++){
+
+				grid[i][j] = Character.getNumericValue(line.charAt(j));
+
+			}
+
+		}
+
+		return grid;
+
+	}
+
+	private int countHiddenTrees(int[][] grid){
+
+		int sum = grid.length*grid[0].length;
+
+		for (int i = 1; i < grid.length-1; i++){
+
+			for (int j = 1; j < grid[0].length-1; j++){
+
+				boolean[] hidden = new boolean[4];
+
+				int check = grid[i][j];
+
+				// up
+
+				for (int k = i-1; k >= 0; k--){
+
+					if (grid[k][j] >= check){
+						hidden[0] = true;
+						break;
+					}
+
+				}
+
+				// left
+
+				for (int k = j-1; k >= 0; k--){
+
+					if (grid[i][k] >= check){
+						hidden[1] = true;
+						break;
+					}
+
+				}
+
+				// right
+
+				for (int k = j+1; k < grid[0].length; k++){
+
+					if (grid[i][k] >= check){
+						hidden[2] = true;
+						break;
+					}
+
+				}
+
+				// down
+
+				for (int k = i+1; k < grid.length; k++){
+
+					if (grid[k][j] >= check){
+						hidden[3] = true;
+						break;
+					}
+
+				}
+
+				if ( (hidden[0] && hidden[1] && hidden[2] && hidden[3])){
+
+					sum--;
 	
+				}
+
+			}
+		
+		}
+
+		return sum;
+
+	}
+
 	private void part2(ArrayList<String> data){
+
+		int sum = 0;
+		int[][] grid = parseData(data);
+
+		sum = calculateScenicScore(grid);
+
+		System.out.println("The scenic score is: " + sum);
+
+	}	
+
+	private int calculateScenicScore(int[][] grid){
+
+		ArrayList<Integer> scores = new ArrayList<>();
+
+		for (int i = 1; i < grid.length-1; i++){
+
+			for (int j = 1; j < grid[0].length-1; j++){
+
+				int check = grid[i][j];
+
+				int up = 0;
+				int down = 0;
+				int left = 0;
+				int right = 0;
+
+				// up
+
+				for (int k = i-1; k >= 0; k--){
+
+					up++;
+
+					if (grid[k][j] >= check){
+						break;
+					}
+
+				}
+
+				// left
+
+				for (int k = j-1; k >= 0; k--){
+
+					left++;
+
+					if (grid[i][k] >= check){
+						break;
+					}
+
+				}
+
+				// right
+
+				for (int k = j+1; k < grid[0].length; k++){
+
+					right++;
+
+					if (grid[i][k] >= check){
+						break;
+					}
+
+				}
+
+				// down
+
+				for (int k = i+1; k < grid.length; k++){
+
+					down++;
+
+					if (grid[k][j] >= check){
+						break;
+					}
+
+				}
+
+				scores.add(up*left*right*down);
+
+			}
+		
+		}
+
+		return Collections.max(scores);
 
 	}
 
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-	static int day = 7;
+	static int day = 8;
 
 	static StringBuilder inputpath = new StringBuilder();
 	static StringBuilder testpath = new StringBuilder();
