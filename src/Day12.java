5e085ef9f02a68f3767843c3146ed659e6f0cf59
Nina Høegh-Larsen <ninius@online.no>
2022-12-12 06:37:00 +0100

    Structure set up, won't finish in time for work

--- src/Day12.java
+++ src/Day12.java
@@ -18,7 +18,7 @@ public class Day12 {
 		System.out.println();
 
 		part1(testinput);
-		part1(input);
+		//part1(input);
 
 		System.out.println();
 		System.out.println("Part 2: ");
@@ -31,6 +31,95 @@ public class Day12 {
 
 	private void part1(ArrayList<String> data){
 
+		int path = 0;
+
+		path = parseData(data);
+
+		System.out.println("The shortest path is: " + path);
+
+	}
+
+	private int parseData(ArrayList<String> data){
+
+		int x = data.get(0).length();
+		int y = data.size();
+		int travel = Integer.MAX_VALUE;
+
+		int[][] grid = Library.initArray(x, y);
+		int[][] path = Library.initArray(x, y);
+
+		fillGrids(grid, path, x, y, data);
+		
+		printGrids(grid, path, x, y);
+
+		travel = findShortestPath(grid, path, x, y);
+
+		return travel;
+
+	}
+
+	private void fillGrids(int[][] grid, int[][] path, int x, int y, ArrayList<String> data){
+
+		for (int i = 0; i < y; i++){
+
+			String line = data.get(i);
+
+			for (int j = 0; j < x; j++){
+				grid[i][j] = line.charAt(j);
+				path[i][j] = '.';
+			}
+
+		}
+
+	}
+
+	private void printGrids(int[][] grid, int[][] path, int x, int y){
+
+		System.out.println();
+
+		for (int i = 0; i < y; i++){
+
+			for (int j = 0; j < x; j++){
+				System.out.print ((char)grid[i][j]);
+			}
+
+			System.out.println();
+		}
+
+		System.out.println();
+
+		for (int i = 0; i < y; i++){
+
+			for (int j = 0; j < x; j++){
+				System.out.print ((char)path[i][j]);
+			}
+
+			System.out.println();
+		}
+
+		System.out.println();
+
+	}
+
+	private int findShortestPath(int [][] grid, int[][] path, int x, int y){
+
+		int travel = findPathRecursive(grid, path, x, y);
+
+		return travel;
+
+	}
+
+	private int findPathRecursive(int [][] grid, int[][] path, int x, int y){
+
+		int travel = Integer.MAX_VALUE;
+
+		if (travel > 0){
+			return 0;
+		}
+
+		travel = findPathRecursive(grid, path, x, y);
+
+		return travel;
 	}
 
 	private void part2(ArrayList<String> data){
