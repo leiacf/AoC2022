0b54b2447e7f13ae6b91681b3702ab443f9da80b
Nina Høegh-Larsen <ninius@online.no>
2022-12-14 09:32:28 +0100

    Day 14 part 1 done, part 2 bogus

---
+++ src/Cave.java
@@ -0,0 +1,142 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.Point;
+
+public class Cave {
+    
+    private HashMap<Point, String> structure = new HashMap<>();
+    private Point entry = new Point(500, 0);
+    private int floor = 0;
+ 
+    Cave(){
+
+    }
+
+    protected void addRock(int x1, int y1, int x2, int y2){
+
+        if (y1 == y2){
+
+            for (int i = x1; i <= x2; i++){
+                Point temp = new Point(i, y1);
+                structure.put(temp, "#");
+            }
+
+        } else if (x1 == x2) {
+
+            for (int i = y1; i <= y2; i++){
+                Point temp = new Point(x1, i);
+                structure.put(temp, "#");
+            }
+
+        }
+
+    }
+
+    protected void addFloor(int floor, int x1, int x2){
+
+        this.floor = floor+2;
+
+        for (int i = x1; i <= x2; i++){
+            Point temp = new Point(i, floor);
+            structure.put(temp, "#");
+        }
+
+
+
+    }
+
+
+    protected int sandStorm(boolean part2){
+
+        boolean room = true;
+
+        while (room){
+
+            room = fall(part2);
+            
+        }
+
+        return findSand();
+
+    }
+
+    private boolean fall(boolean part2){
+
+        int y = (int)entry.getY();
+        int x = (int)entry.getX();
+
+        Point sand = new Point(x, y);
+   
+        while (true){
+
+            if ((int)sand.getY() == 1000){
+                return false;
+            }
+
+            if (structure.get(sand) == null){
+                
+                y++;
+                sand.setLocation(x, y);
+
+            } else {
+                
+                // blocked, try left
+
+                x--;
+                sand.setLocation(x, y);
+
+                if (structure.get(sand) != null){
+                
+                    // blocked, try right
+                    
+                    x+=2;
+                    sand.setLocation(x, y);
+
+                    if (structure.get(sand) != null){
+
+                        // blocked, add sand
+                        
+                        x--;
+                        y--;
+
+                        sand.setLocation(x, y);
+
+                        if (sand.equals(entry)){
+                            // cant add more sand
+                            return false;
+                        }
+                        
+                        structure.put(sand, "o");
+
+                        return true;
+
+                    }
+                    
+                }
+
+            }
+
+        }
+
+    }
+
+    private int findSand(){
+
+        int sum = 0;
+
+        for(Map.Entry<Point, String> entry : structure.entrySet()) {
+            
+            String value = entry.getValue();
+        
+            if (value.equals("o")){
+                sum++;
+            }
+        }
+
+        return sum;
+
+    }
+
+    
+
+}
--- src/Day14.java
+++ src/Day14.java
@@ -31,9 +31,70 @@ public class Day14 {
 
 	private void part1(ArrayList<String> data){
 
+		int sum = parseData(data, false);
+
+		System.out.println("The amount of sand that comes to rest is: " + sum);
+
+	}
+
+	private int parseData(ArrayList<String> data, boolean part2){
+
+		Cave cave = new Cave();
+		int maxY = 0;
+		int minX = Integer.MAX_VALUE;
+		int maxX = 0;
+
+		for (String line : data){
+
+			String[] times = line.split(" -> ");
+
+			for (int i = 0; i < times.length-1; i++){
+
+				String[] first = times[i].split(",");
+				String[] second = times[i+1].split(",");
+
+				int x1 = Integer.parseInt(first[0]);
+				int y1 = Integer.parseInt(first[1]);
+				int x2 = Integer.parseInt(second[0]);
+				int y2 = Integer.parseInt(second[1]);
+
+				if (x1 > x2){
+
+					int x3 = x2;
+					x2 = x1;
+					x1 = x3;
+	
+				}
+
+				if (y1 > y2){
+
+					int y3 = y2;
+					y2 = y1;
+					y1 = y3;
+	
+				}
+
+				if (y2 > maxY) { maxY = y2; }
+				if (x1 < minX) { minX = x1; }
+				if (x2 > maxX) { maxX = x2; }
+
+				cave.addRock(x1, y1, x2, y2);
+
+			}
+
+		}
+
+		if (part2) {cave.addFloor(maxY, minX-10, maxX+10); }
+
+		return cave.sandStorm(part2);
+
 	}
 
 	private void part2(ArrayList<String> data){
+
+		int sum = parseData(data, true);
+
+		System.out.println("The amount of sand that comes to rest is: " + sum);
 
 	}
 
