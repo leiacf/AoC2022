030a94e28d632dd35bcd0c0a24f2073d8bf37838
Nina Høegh-Larsen <ninius@online.no>
2022-12-19 11:58:59 +0100

    Day 15 part 2 - Fast, but not really my idea

--- src/Day15.java
+++ src/Day15.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.awt.Point;
@@ -27,8 +28,8 @@ public class Day15 {
 		System.out.println("Part 2: ");
 		System.out.println();
 
-		part2(testinput);
-		part2(input);
+		part2(testinput, 20);
+		part2(input, 4000000);
 
 	}
 
@@ -110,8 +111,7 @@ public class Day15 {
 		long sum = 0;
 
 		for(Map.Entry<Point, String> entry : map.entrySet()) {
-        
-			Point key = entry.getKey();
+
 			String value = entry.getValue();
 
             if (value.equals("#")){
@@ -140,7 +140,198 @@ public class Day15 {
 
 	}
 
-	private void part2(ArrayList<String> data){
+	private void part2(ArrayList<String> data, int max){
+
+		int min = 0;
+		long mul = 4000000;
+		long tuning = 0;
+
+		ArrayList<Sensor> sensors = new ArrayList<>();
+		ArrayList<int[]> ranges = new ArrayList<>();
+
+		parseSensors(data, sensors);
+		
+		for (int i = min; i <= max; i++){
+
+			ranges.clear();
+
+			findRanges(sensors, i, ranges, min, max);
+			boolean end = combineRanges(ranges, min, max);
+				
+			if (end){
+				long hole = getHole(ranges);
+				tuning = (hole*mul) + i;
+				break;
+			}	
+		}
+
+		System.out.println("The tuning frequency is: " + tuning);
+
+	}
+
+	private void findRanges(ArrayList<Sensor> sensors, int y, ArrayList<int[]> ranges, int min, int max) {
+
+		for (Sensor sensor : sensors) {
+			
+			Point s = sensor.getSensor();
+			int manhattan = sensor.getManhattan();
+
+			int sX = (int) s.getX();
+			int sY = (int) s.getY();
+
+			int dist = Math.abs(sY - y);
+			
+			if (dist <= manhattan){
+
+				int rest = manhattan-dist;
+				int xMin = sX - rest;
+				int xMax = sX + rest;
+
+				if (xMax > min && xMin < max){
+
+					int[] temp = new int[2];
+
+					temp[0] = xMin;
+					temp[1] = xMax;
+	
+					if (temp[0] < min) { temp[0] = min; }
+					if (temp[1] > max) { temp[1] = max; }
+	
+					ranges.add(temp);
+
+				}
+
+			}
+
+		}
+
+	}
+
+	private boolean combineRanges(ArrayList<int[]> ranges, int min, int max){
+
+		ArrayList<Integer> removal = new ArrayList<>();
+
+		findContained(ranges, removal);
+		remove(ranges, removal);
+
+		findOverlap(ranges, removal);
+		remove(ranges, removal);
+
+		findOverlap(ranges, removal);
+		remove(ranges, removal);
+
+		int sum = 0;
+
+		for (int i = 0; i < ranges.size(); i++){
+
+			int[] temp = ranges.get(i);
+
+			sum += temp[1] - temp[0];
+
+		}
+
+		if (sum == max){
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	private void remove(ArrayList<int[]> ranges, ArrayList<Integer> removal){
+
+		Collections.sort(removal);
+		Collections.reverse(removal);
+
+		for (int i = 0; i < removal.size(); i++){
+			ranges.remove((int)removal.get(i));
+		}
+
+		removal.clear();
+
+	}
+
+	private void findOverlap(ArrayList<int[]> ranges, ArrayList<Integer> removal){
+
+		for (int i = 0; i < ranges.size(); i++){
+			for (int j = i+1; j < ranges.size(); j++){	
+
+				int[] first = ranges.get(i);
+				int[] second = ranges.get(j);
+
+				int a = first[0];
+				int b = first[1];
+				int c = second[0];
+				int d = second[1];
+
+				if ((a <= c) && (b >= c) && (b <= d)){
+					
+					first[0] = a;
+					first[1] = d;
+					ranges.set(i, first);
+					if (! removal.contains(j)){
+						removal.add(j);	
+					}
+				
+				} else if ((a >= c) && (a <= d) && (b >= d)){
+					
+					first[0] = c;
+					first[1] = b;
+
+					ranges.set(i, first);
+
+					if (! removal.contains(j)){
+						removal.add(j);	
+					}
+					
+				}
+
+			}
+		}
+
+	}
+
+	private void findContained(ArrayList<int[]> ranges, ArrayList<Integer> removal){
+
+		for (int i = 0; i < ranges.size(); i++){
+			for (int j = i+1; j < ranges.size(); j++){
+
+				int[] first = ranges.get(i);
+				int[] second = ranges.get(j);
+
+				if (first[0] >= second[0] && first[1] <= second[1]){
+
+					if (! removal.contains(i)){
+						removal.add(i);	
+					}
+					
+				} else if (second[0] > first[0] && second[1] < first[1]){
+					if (! removal.contains(j)){
+						removal.add(j);	
+					}
+				}
+
+			}
+
+		}
+
+
+	}
+
+	private long getHole(ArrayList<int[]> ranges){
+
+		long hole = 0;
+
+		if (ranges.size() > 1){
+
+			long a = ranges.get(0)[1];
+			long b = ranges.get(1)[0];
+
+			hole = (a+b) / 2;
+
+		}
+
+		return hole;
 
 	}
 
